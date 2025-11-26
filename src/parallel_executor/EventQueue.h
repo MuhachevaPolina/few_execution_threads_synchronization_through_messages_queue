9baddd3d2347b21915d3c96d7117eb78a9dd0a7a
MuhachevaPolina <muhachevapolina@icloud.com>
2025-11-26 14:39:55 +0300

    added files for class EventQueue

---
+++ src/parallel_executor/EventQueue.h
@@ -0,0 +1,25 @@
+class EventQueue
+
+{
+
+public:
+
+//  put message into queue
+
+void push(const std::shared_ptr<const Event>& event);
+
+ 
+
+// delete message from queue and return it. if queue is empty and 
+// after the duration return empty pointer
+        std::shared_ptr<const Event> pop(const std::chrono::seconds& duration);
+
+private:
+
+	std::queue<std::shared_ptr<const Event>> queue;
+
+	std::mutex mtx;
+
+	std::condition_variable cv;
+
+};
