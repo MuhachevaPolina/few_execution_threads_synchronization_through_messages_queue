6809caeef144939eff5e6bb1b7bfae1f4fe79979
MuhachevaPolina <muhachevapolina@icloud.com>
2026-01-23 14:04:34 +0300

    deleted build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/template/106-cpp-parallel-pmuhacheva
-# Build directory: /home/template/106-cpp-parallel-pmuhacheva/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/template/106-cpp-parallel-pmuhacheva/test
-# Build directory: /home/template/106-cpp-parallel-pmuhacheva/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(DeviceTests "/home/template/106-cpp-parallel-pmuhacheva/build/test/test_device")
-set_tests_properties(DeviceTests PROPERTIES  _BACKTRACE_TRIPLES "/home/template/106-cpp-parallel-pmuhacheva/test/CMakeLists.txt;21;add_test;/home/template/106-cpp-parallel-pmuhacheva/test/CMakeLists.txt;0;")
